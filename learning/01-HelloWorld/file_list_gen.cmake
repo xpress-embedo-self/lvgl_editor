b4e0c3d8de3d1209c7b452f89d95768368477371
xpress-embedo-self <self.arunsharma07@gmail.com>
2025-11-29 19:40:26 +0530

    base files added for learning

---
+++ learning/01-HelloWorld/file_list_gen.cmake
@@ -0,0 +1,7 @@
+list(
+  APPEND
+  PROJECT_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_hello_world_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_hello_world.c
+  ${CMAKE_CURRENT_LIST_DIR}/fonts/font_medium_data.c)
