e6e7e1800f7549ccfe729f058a94e208e4f6a21e
xpress-embedo-self <self.arunsharma07@gmail.com>
2025-11-29 19:44:21 +0530

    adding files for new project

---
+++ learning/1_hello_world/ui_hello_world_gen.h
@@ -0,0 +1,89 @@
+/**
+ * @file ui_hello_world_gen.h
+ */
+
+#ifndef UI_HELLO_WORLD_GEN_H
+#define UI_HELLO_WORLD_GEN_H
+
+#ifndef UI_SUBJECT_STRING_LENGTH
+#define UI_SUBJECT_STRING_LENGTH 256
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+    #include "lvgl.h"
+#else
+    #include "lvgl/lvgl.h"
+#endif
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL VARIABLES
+ **********************/
+
+/*-------------------
+ * Permanent screens
+ *------------------*/
+
+/*----------------
+ * Global styles
+ *----------------*/
+
+/*----------------
+ * Fonts
+ *----------------*/
+
+extern lv_font_t * font_medium;
+
+/*----------------
+ * Images
+ *----------------*/
+
+/*----------------
+ * Subjects
+ *----------------*/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/*----------------
+ * Event Callbacks
+ *----------------*/
+
+/**
+ * Initialize the component library
+ */
+
+void ui_hello_world_init_gen(const char * asset_path);
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   POST INCLUDES
+ **********************/
+
+/*Include all the widget and components of this library*/
+#include "screens/screen_hello_world_gen.h"
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*UI_HELLO_WORLD_GEN_H*/
